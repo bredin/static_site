cb6483e0eba731f406ef28ce8bc795d9e1157d5f
bredin <insanityindustries@gmail.com>
2025-08-11 16:47:29 +1200

    leafnode added

---
+++ src/htmlnode.py
@@ -0,0 +1,29 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children if children is not None else []
+        self.props = props if props is not None else {}
+
+    def to_html(self):
+        raise NotImplementedError("Subclasses should implement this method.")
+
+    def props_to_html(self):
+        return ' '.join(f'{key}="{value}"' for key, value in self.props.items())
+
+    def __repr__(self):
+        return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value=value, children=None, props=props)
+        if value is None:
+            raise ValueError("LeafNode must have a value.")
+
+    def to_html(self):
+        if self.tag is None:
+            return self.value
+        props_html = self.props_to_html()
+        if props_html:
+            return f"<{self.tag} {props_html}>{self.value}</{self.tag}>"
+        return f"<{self.tag}>{self.value}</{self.tag}>"
